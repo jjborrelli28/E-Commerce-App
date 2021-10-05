a855523cdd7148627ae9ba11cfd53d0a56e3fe58
Juan <juanjoseborrelli92@outlook.com>
2021-10-05 13:33:11 -0300

    Primer fetch a la API

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useFetch } from "../../hooks/useFetch";
 
 export const HomeScreen = () => {
+  const url = `https://fravega.myvtex.com/api/catalog_system/pub/products/search/notebook`;
+
+  const { data, loading, error } = useFetch(url);
+
+  console.log(data);
   return (
     <div>
       <h1>HomeScreen</h1>
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+  const [state, setState] = useState({
+    data: null,
+    loading: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) =>
+        setState({
+          data,
+          loading: null,
+          error: null,
+        })
+      )
+      .catch((error) => setState({ data: null, loading: null, error }));
+  }, [url]);
+
+  return state;
+};
