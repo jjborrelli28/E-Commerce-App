d889e561656a3c1366aabdd839045d6b81961678
Juan <juanjoseborrelli92@outlook.com>
2021-11-02 11:54:03 -0300

    Size Input

--- src/components/Shared/InputSearch.js
+++ src/components/Shared/InputSearch.js
@@ -4,6 +4,7 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { BsSearch } from "react-icons/bs";
 import { useSearch } from "../../hooks/useSearch";
 
@@ -12,24 +13,28 @@ export const InputSearch = () => {
 
   return (
     <Row xs={1} md={2} className="g-4 justify-content-md-center">
-      <Form className="form-container" onSubmit={handleInputSearch}>
-        <InputGroup className="mt-5">
-          <FormControl
-            placeholder="Buscar productos"
-            aria-label="Buscar productos"
-            aria-describedby="basic-addon2"
-            value={value}
-            onChange={handleInputChange}
-          />
-          <Button
-            variant="primary"
-            id="button-addon2"
-            onClick={handleInputSearch}
-          >
-            <BsSearch />
-          </Button>
-        </InputGroup>
-      </Form>
+      <Col lg={3}></Col>
+      <Col lg={6}>
+        <Form className="form-container" onSubmit={handleInputSearch}>
+          <InputGroup className="mt-5">
+            <FormControl
+              placeholder="Buscar productos"
+              aria-label="Buscar productos"
+              aria-describedby="basic-addon2"
+              value={value}
+              onChange={handleInputChange}
+            />
+            <Button
+              variant="primary"
+              id="button-addon2"
+              onClick={handleInputSearch}
+            >
+              <BsSearch />
+            </Button>
+          </InputGroup>
+        </Form>
+      </Col >
+      <Col lg={3}></Col>
     </Row>
   );
 };
